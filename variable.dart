3a0ff7c7b935d6d1eefa183fddd303f5895a1b04
Sohel Mia <sohelcse1999@gmail.com>
2021-04-03 01:57:56 +0600

    Dart variable declarations

---
+++ variable.dart
@@ -0,0 +1,23 @@
+void main()
+{
+  //Number : int
+  int score = 10;
+  var count = 20; //It is inferred as integer automatically.
+  print(score);
+  int hexValue = 0xFFFFFF;
+  print(hexValue);
+
+  //Number :double
+    double amount = 10.2;
+    print(amount);
+
+    //String;
+    String countryName = 'Bangladesh';
+    print(countryName);
+
+    //Boolean
+    bool isLikable = false;
+    print(isLikable);
+
+    //Note :All data type in dart are objects.
+}
